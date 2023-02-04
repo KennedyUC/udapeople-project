921e769c4a52f999cf45fc3a978cba918b9f3361
kennedyuc <chukwukakennedyuche@gmail.com>
2023-02-04 03:17:09 -0800

    udapeople project init commit

---
+++ .circleci/ansible/roles/configure-server/files/env.sh
@@ -0,0 +1,9 @@
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_HOST=localhost #TODO: update
+export TYPEORM_PORT=5532
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=mypassword #TODO: update
+export TYPEORM_DATABASE=postgres
